8e18a2776f4fadc47fa02475d9518fe18b56ede9
NicHub <nicolas.jeanmonod@gmail.com>
2016-09-20 10:40:15 +0200

    traduction

--- docs/assets/js/soon/custom.js
+++ docs/assets/js/soon/custom.js
@@ -29,18 +29,18 @@ COMMING SOON PAGE
         else{
             var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
             var d = Math.floor(s/86400);
-            days.html('<h1>'+d+'</h1><p>Jours'+(d>1?'s':''),'</p>');
+            days.html('<h1>'+d+'</h1><p>Jour'+(d>1?'s':''),'</p>');
             s -= d*86400;
 
             var h = Math.floor(s/3600);
-            hours.html('<h1>'+h+'</h1><p>Heures'+(h>1?'s':''),'</p>');
+            hours.html('<h1>'+h+'</h1><p>Heure'+(h>1?'s':''),'</p>');
             s -= h*3600;
 
             var m = Math.floor(s/60);
-            minutes.html('<h1>'+m+'</h1><p>Minutes'+(m>1?'s':''),'</p>');
+            minutes.html('<h1>'+m+'</h1><p>Minute'+(m>1?'s':''),'</p>');
 
             s = Math.floor(s-m*60);
-            seconds.html('<h1>'+s+'</h1><p>Secondes'+(s>1?'s':''),'</p>');
+            seconds.html('<h1>'+s+'</h1><p>Seconde'+(s>1?'s':''),'</p>');
             setTimeout(setDate, 1000);
 
             message.html('MAIS PAS SI VITE QUE ÇA QUAND MÊME');
